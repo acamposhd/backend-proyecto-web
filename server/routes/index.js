63d6b9f1ff4e961e47dbceaba5d7b1c00a8c2a59
Alberto Campos <albertocamposhdez@hotmail.com>
2020-04-07 00:44:09 -0500

    Se agrega ruta /curso

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,5 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use('/persona', require('./persona/usuario'));
+app.use('/curso', require('./persona/usuario'));
+
 
 module.exports = app;
