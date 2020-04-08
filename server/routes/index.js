9ee434ba84d8cb8dbcf002a01d33223b9d2654c1
Maria Fernanda Madrigal Morales <st1469@utr.edu.mx>
2020-04-07 22:25:48 -0500

    Se agrego la ruta de userCourses

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,7 @@ const app = express();
 
 app.use('/persona', require('./persona/usuario'));
 app.use('/curso', require('./courses/curso'));
+app.use('/userCourses', require('./userCourses/usuarioCurso'));
 
 
 module.exports = app;
